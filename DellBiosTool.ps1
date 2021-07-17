260e5ac005a4e9e66907a8e261b6ab371b130038
Dillon Pratt | bx9090 <pratt.dillon5@gmail.com>
2021-07-17 11:48:02 -0600

    Added some comments
    Still could use better documentation + code review. Will do so on the next version / if this tool is getting use out of people. Please submit issues / requests for updates for code reviews and updates to become a reality

--- DellBiosTool.ps1
+++ DellBiosTool.ps1
@@ -1,6 +1,14 @@
-﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+#Elevate to Administrator status if not already.
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { 
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
 write-host "Loading..." -ForegroundColor Green
 
+<#
+.Description
+Get_Dell_BIOS_Settings will check if the DellBiosProvider module is installed, install it if it's not.
+.PARAMETER
+#>
 Function Get_Dell_BIOS_Settings
 {
     $i = 0
@@ -23,7 +31,7 @@ Function Get_Dell_BIOS_Settings
         Description = $_."Description"    
         "Possible Values" = $_."PossibleValues"
         PSPath = $_."PSPath"
-        UID = $i++                                                 
+        UID = $i++   # unique ID number for each cmd in list                                              
         }}          
                 
     If($ShowDescription)
@@ -36,19 +44,33 @@ Function Get_Dell_BIOS_Settings
         }
 }  
 
-function refresh($pth) {
+<#
+.Description
+Refreshes the available BIOS settings and their values to be relisted
+.PARAMETER
+#>
+function refresh() {
     write-host "Refreshing BIOS settings" -ForegroundColor DarkMagenta
     $global:settings = $null
     $global:settings = Get_Dell_BIOS_Settings
 }
 
 
-
+<#
+.Description
+Clean the prompt and prompt2 variables to $null
+.PARAMETER
+#>
 function clean() {
     $prompt = $null
     $prompt2 = $null
 }
 
+<#
+.Description
+
+.PARAMETER
+#>
 function Selected() {
     $selected = ($global:settings[$prompt]).Setting
     $PSPath = ((($global:settings[$prompt]).PSPath).substring(33) + ("\"+$selected))
@@ -77,24 +99,35 @@ function Selected() {
         } 
         while($prompt2 -notmatch "[120]")
 }
+
+<#
+.Description
+Displays all settings to screen and prompts user to enter a setting ID
+.PARAMETER
+#>
 function Menu() {
     $r=0
     foreach($entry in $global:settings) {
         write-host "["$r "]:"($entry).Setting
         $r++
     }
-
-        do{
-    
+    do{
         $Prompt = Read-host "Enter a number (or 'quit')"
         if($prompt -eq 'quit') {exit(1)}
-          if(([int]$prompt -lt $r) -and ([int]$Prompt -gt -1)) {Selected}
-          
+            if(([int]$prompt -lt $r) -and ([int]$Prompt -gt -1)) {Selected}
             else {write-host "`nInvalid Entry`n" -ForegroundColor Red; continue}
         }
-        while(!(([int]$prompt -lt $r) -and ([int]$Prompt -gt -1)))
+    while(!(([int]$prompt -lt $r) -and ([int]$Prompt -gt -1)))
 }
 
+<#
+.Description
+Displays the selected settings details
+.PARAMETER
+UID = Unique ID given to the setting selected
+PSPATH = Path to the location of the bios setting
+count = This ensures that a single selection was made by the menu
+#>
 function ViewDetails($UID, $PSPATH, $count) {
     if($count -eq 0) {
         write-host "Viewing Details for:" $global:settings[$prompt].Setting -ForegroundColor Gray
@@ -120,6 +153,15 @@ function ViewDetails($UID, $PSPATH, $count) {
     Selected
 }
 
+<#
+.Description
+Updates the selected bios setting to the new value. Refreshes list.
+.PARAMETER
+att = new BIOS value
+pth = PSPath to Bios Setting
+val = Old value of Setting
+uid = Unique ID of setting selected
+#>
 function EditBIOS($att, $pth, $val, $uid) {
     write-host "Writing value:"$val" to "$att -ForegroundColor Gray
     $fixedpath = ($pth.substring(33) + ("\"+$att))
@@ -130,12 +172,19 @@ function EditBIOS($att, $pth, $val, $uid) {
     Selected
 }
 
+<#
+.Description
+Interface for updating a Bios Setting
+.PARAMETER
+UID = Unique ID for Setting Selected
+#>
 function EditSetting($UID) {
     write-host "Changing Setting for: "($global:settings[$UID]).Setting -ForegroundColor Yellow
     write-host "Current status: " $global:settings[$UID].Value -ForegroundColor Yellow
     $i = 1
     $optionsArray = @()
     [System.Collections.ArrayList]$optionsArraycomplete = @()
+    # Collects every possible value allowed to be entered for the selected setting
     foreach($entry in (($global:settings[$UID]))) { 
             try {(($entry.'Possible Values').Split(" ") | Foreach { $optionsArray += "$i"; $optionsArray += "$_"; $i++})}
             catch { continue }
@@ -190,7 +239,13 @@ function EditSetting($UID) {
 }
 
 
-
+<#
+.Description
+Validates that the setting has been updated
+.PARAMETER
+pth = PSPath of the setting that was updated
+val = the value the setting should now be
+#>
 function Validation($pth, $val) {
     $newVal = Get-Item -Path $pth | Select CurrentValue
     IF($newVal.CurrentValue -ne $val) { write-host "Error updating value" -ForegroundColor Red; continue }
@@ -198,6 +253,7 @@ function Validation($pth, $val) {
 
 }
 
+#starts here
 refresh
 Menu
 
